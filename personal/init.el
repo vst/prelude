3dd7b2e1f738e45ac8cd5a7d3f43a3276ac2568e
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2018-12-12 17:29:23 +0300

    Enable deft

--- personal/init.el
+++ personal/init.el
@@ -1,2 +1,7 @@
 ;; We want the daemon mode:
 (server-start)
+
+(prelude-require-package 'deft)
+
+(setq deft-extensions '("txt" "tex" "org" "md"))
+(setq deft-directory "~/repos/playground/defter/")
