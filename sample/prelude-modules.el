109046d0752bb642dbbc7214555308fb61005f73
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2019-01-09 19:48:08 +0800

    Enable haskell module

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -20,7 +20,7 @@
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-elixir)
 ;; (require 'prelude-go)
-;; (require 'prelude-haskell)
+(require 'prelude-haskell)
 (require 'prelude-js)
 ;; (require 'prelude-latex)
 (require 'prelude-lisp)
