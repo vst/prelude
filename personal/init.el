f30171f71ced5be4d4841ac57e45432af6d81fbc
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2019-01-12 11:32:55 +0800

    Add lsp-mode/lsp-python + friends

--- personal/init.el
+++ personal/init.el
@@ -1,7 +1,7 @@
 ;; We want the daemon mode:
 (server-start)
 
-(prelude-require-package 'deft)
+(prelude-require-packages '(deft lsp-mode lsp-python lsp-ui company-lsp pyenv-mode))
 
 (setq deft-extensions '("txt" "tex" "org" "md"))
 (setq deft-directory "~/repos/playground/defter/")
