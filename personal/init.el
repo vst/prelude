1414c8df596a7460fa2133033406f43154422165
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2019-02-02 21:30:35 +0800

    Trying lsp-haskell

--- personal/init.el
+++ personal/init.el
@@ -1,7 +1,15 @@
 ;; We want the daemon mode:
 (server-start)
 
-(prelude-require-packages '(deft lsp-mode lsp-python lsp-ui company-lsp pyenv-mode))
+(prelude-require-packages '(deft lsp-mode lsp-python lsp-ui company-lsp pyenv-mode lsp-mode lsp-haskell))
+
+(add-hook 'haskell-mode-hook #'lsp)
+(setq lsp-haskell-process-path-hie "hie-wrapper")
+
+(require 'lsp)
+(require 'lsp-haskell)
+(add-hook 'haskell-mode-hook #'lsp)
+
 
 (setq deft-extensions '("txt" "tex" "org" "md"))
 (setq deft-directory "~/repos/playground/defter/")
