c3fc7dad189b9a150931700a00c61eb979de5da1
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2019-02-16 11:47:50 +0800

    Fixing lsp-haskell (peh)

--- personal/init.el
+++ personal/init.el
@@ -3,12 +3,12 @@
 
 (prelude-require-packages '(deft lsp-mode lsp-python lsp-ui company-lsp pyenv-mode lsp-mode lsp-haskell))
 
-(add-hook 'haskell-mode-hook #'lsp)
-(setq lsp-haskell-process-path-hie "hie-wrapper")
 
 (require 'lsp)
 (require 'lsp-haskell)
 (add-hook 'haskell-mode-hook #'lsp)
+(setq lsp-haskell-process-path-hie "hie-wrapper")
+(setq haskell-saved-check-command "hlint")
 
 
 (setq deft-extensions '("txt" "tex" "org" "md"))
